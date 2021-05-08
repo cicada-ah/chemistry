cc33b65d73bea022b2d06bae5e51fb429b7e55dd
guosanchuan <guosanchuan@baijiahulian.com>
2021-05-08 13:40:26 +0800

    feat: xietiansb

--- src/component/Panel.svelte
+++ src/component/Panel.svelte
@@ -1,9 +1,10 @@
-<script lang="typescript">
+<script lang="ts">
     import { onMount, setContext } from "svelte";
-    import { Konva, key } from "../Konva"
-    let stage
-    let layer
-    let con
+    import { Konva, key } from "../Konva";
+    import { haveIntersection } from "@/utils/utilsFunc.ts";
+    let stage;
+    let layer;
+    let con;
 
     setContext(key, {
         getStage() {
@@ -13,9 +14,10 @@
             return layer;
         },
         getContainer() {
-            return con
-        }
-    })
+            return con;
+        },
+    });
+
     onMount(() => {
         const width = window.innerWidth;
         const height = window.innerHeight;
@@ -34,7 +36,26 @@
             e.preventDefault();
             stage.setPointersPositions(e);
             // console.log()
-            Konva.Image.fromURL(e.dataTransfer.getData("text"), function (image: Konva.Image) {
+            Konva.Image.fromURL(e.dataTransfer.getData("text"), function (
+                image: Konva.Image & { _testRotate: Function }
+            ) {
+                image._testRotate = (deg: number) => {
+                    var tween = new Konva.Tween({
+                        node: image,
+                        duration: 2,
+                        rotation: deg,
+                        opacity: 0.5,
+                        easing: Konva.Easings.BackEaseOut,
+                        onFinish: function () {
+                            image.destroy();
+                        },
+                    });
+
+                    setTimeout(function () {
+                        tween.play();
+                    }, 100);
+                };
+
                 layer.add(image);
 
                 image.position(stage.getPointerPosition());
@@ -43,11 +64,71 @@
                 layer.draw();
             });
         });
+        layer.on("dragmove", function (e) {
+            let target = e.target;
+            let targetRect = target.getClientRect();
+            layer.children.each((group) => {
+                if (group === target) {
+                    return;
+                }
+                if (haveIntersection(group.getClientRect(), targetRect)) {
+                    const { x, y, width, height } = group.getClientRect();
+                    target.position({
+                        x: x + 25,
+                        y,
+                    });
+                    target.draggable(false);
+                    target._testRotate(-70);
+
+                    let A = 3,
+                        W = 1 / 3,
+                        Q = 0,
+                        H = height / 2,
+                        speed = -0.001;
+                    var triangle = new Konva.Shape({
+                        x: x,
+                        y: y,
+                        sceneFunc: function (ctx, shape) {
+                            (function drawAnimation() {
+                                ctx.beginPath();
+                                ctx.moveTo(0, height / 2);
+                                Q += speed;
+                                for (let xm = 0; xm <= width; xm++) {
+                                    let ym = A * Math.sin(W * xm + Q) + H;
+                                    ctx.lineTo(xm, ym);
+                                }
+                                ctx.stroke();
+                                // ctx.lineTo(width - 13, H);
+                                // ctx.lineTo(0, height);
+                                // ctx.fill();
+                                ctx.closePath();
+                                // window.requestAnimationFrame(drawAnimation);
+                            })();
+
+                            // (!) Konva specific method, it is very important
+                            ctx.fillStrokeShape(shape);
+                        },
+                        fill: "#00ffff",
+                        stroke: "#00ffff",
+                        lineWidth: 1,
+                        strokeWidth: 1,
+                    });
+
+                    // add the triangle shape to the layer
+                    layer.add(triangle);
+
+                    // add the layer to the stage
+                    stage.add(layer);
+                }
+            });
+        });
     });
 </script>
-<div id="container" />
+
 <style lang="less">
     #container {
         background-color: #1e90ff;
     }
 </style>
+
+<div id="container" />
